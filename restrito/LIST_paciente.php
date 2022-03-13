814b171ed6aeb5adfb38fd4fb29343b19eb81603
EllenCas172 <ellencas172@gmail.com>
2022-03-13 15:15:13 -0300

    arrumando listagem de paciente

--- restrito/LIST_paciente.php
+++ restrito/LIST_paciente.php
@@ -25,7 +25,6 @@ catch (Exception $e) {
     <meta name="description" content="Página Listagem de Pacientes">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/style_restrito.css">
-    <link rel="stylesheet" href="../css/style_listagem.css">
     <title>Lista de Pacientes</title>
 </head>
 
@@ -65,19 +64,23 @@ catch (Exception $e) {
     <div class="container">
         <main>
             <h2>Listar Pacientes</h2>
-            <table class="table-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Sexo</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col">Endereço</th>
-                    <th scope="col">Peso</th>
-                    <th scope="col">Altura</th>
-                    <th scope="col">Tipo Sanguíneo</th>
-                </tr>
-                <?php
+            <table class="table table-striped">
+                <thead>
+                    <tr class="table-info">
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Sexo</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Endereço</th>
+                        <th scope="col">Peso</th>
+                        <th scope="col">Altura</th>
+                        <th scope="col">Tipo Sanguíneo</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
                     while ($row = $stmt->fetch()) {
                         $id_pessoa = $row['id_pessoa'];
                         $nome = $row['nome'];
@@ -98,17 +101,16 @@ catch (Exception $e) {
                         $peso = $row['peso'];
                         $altura = $row['altura'];
                         $tipoSanguineo = $row['tipoSanguineo'];
-
+                        
                         echo <<<HTML
-                        <tr>
+                        <tr class="table-light">
                             <td>
                                 <a href="EXC_pessoa.php?id_pessoa=$id_pessoa">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-                                </svg></a>
-                            </td>
-                            <td>$id_pessoa</td>
-                            <td>$nome</td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                                    </svg></a>
+                                </td>
+                                <td>$nome</td>
                             <td>$sexo</td>
                             <td>$email</td>
                             <td>$telefone</td>
@@ -119,12 +121,13 @@ catch (Exception $e) {
                         </tr>      
                         HTML;
                     }
-                ?>
+                    ?>
+                </tbody>
             </table>
 
         </main>
     </div>
-
+    
     <!--Rodapé-->
     <footer>
         © Copyright 2021. Todos os direitos reservados.
