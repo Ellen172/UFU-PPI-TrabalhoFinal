a4eb92c1f18d71a7489bbd9c2ff86f76577065b9
Gabriel <gcbsamaral.dev@gmail.com>
2022-03-13 14:58:07 -0300

    Mais mudanças em arquivos de login

--- js/doLogin.js
+++ js/doLogin.js
@@ -7,7 +7,7 @@ function DoLogin() {
 		body: formData
 	}
 
-	fetch("verify_login.php", options)
+	fetch("verifica_login.php", options)
 		.then(response => {
 			if (!response.ok) {
 				throw new Error(response.status);
